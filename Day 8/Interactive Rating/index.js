431964296cea0ce1e78a3c31746ba876d50d8596
amrita150 <amritakataria515@gmail.com>
2023-09-09 00:04:52 +0530

    modified js

--- Day 8/Interactive Rating/index.js
+++ Day 8/Interactive Rating/index.js
@@ -12,16 +12,17 @@ const msg = document.querySelector('[data-ratingInfo]');
 
 
 const ratingButtons = [rateOne, rateTwo, rateThree, rateFour, rateFive];
-console.log(ratingButtons.length);
 
+let selectedRating = 0;
 ratingButtons.forEach((button , index) => {
     button.addEventListener('click' , function(){
-        checkRating(index+1);
+        selectedRating = index + 1;
+        checkRating(selectedRating);
     });
 });
 
+
 function checkRating(selectedRating){
-    console.log(selectedRating);
     for (let index = 1; index <=5; index++) {
         if(index==selectedRating){
             ratingButtons[index-1].classList.add("selectedRating");
@@ -29,10 +30,9 @@ function checkRating(selectedRating){
             ratingButtons[index-1].classList.remove("selectedRating");
         }
     }
-
     if(selectedRating >= 1){
         msg.innerHTML = `You Selected ${selectedRating} out of 5`;
-    } else{
+    } else {
         msg.innerHTML = `You Selected 0 out of 5`;
     }
 }
@@ -41,7 +41,7 @@ const submitBtn = document.querySelector(".btn-cont");
 const ratingBtn = document.querySelector(".rate-btn");
 
 submitBtn.addEventListener('click' , function(){
-    
+    checkRating(selectedRating);
     thankYouCont.classList.add("active");
     submitContainer.classList.remove("active");
 });
